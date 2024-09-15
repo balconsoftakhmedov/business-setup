5fca84d61244998002c1b70f07b8d86a6b51de3b
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-09-15 21:32:03 +0500

    Update enqueue.php

--- includes/enqueue.php
+++ includes/enqueue.php
@@ -7,7 +7,7 @@ if ( ! function_exists( 'vcn_starter_styles_and_scripts' ) && ! is_admin() ) {
 		/*Styles*/
 		wp_enqueue_style( 'starter-style', get_stylesheet_uri(), array(), VCN_THEME_VERSION );
 		wp_enqueue_style( 'starter-base', VCN_TEMPLATE_URI . '/assets/css/style.css', array(), VCN_THEME_VERSION );
-		//wp_enqueue_style( 'google-fonts', starter_theme_fonts(), array(), VCN_THEME_VERSION );
+
 	}
 }
 add_action( 'wp_enqueue_scripts', 'vcn_starter_styles_and_scripts' );
@@ -44,60 +44,15 @@ function vision_prime_enqueue_styles() {
 	// Enqueue Google Fonts
 	wp_enqueue_style(
 		'business-setup-google-fonts-poppins',
-		'https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap',
+		'https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap',
 		array(),
 		null
 	);
 	wp_enqueue_style(
-		'business-setup-google-fonts-merriweather',
-		'https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,400;1,400&display=swap',
+		'business-setup-google-fonts-inter',
+		'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap',
 		array(),
 		null
-	);
-	wp_enqueue_style(
-		'business-setup-google-fonts-dm-sans',
-		'https://fonts.googleapis.com/css2?family=DM+Sans:wght@500;700&display=swap',
-		array(),
-		null
-	);
-	wp_enqueue_style(
-		'business-setup-google-fonts-playfair',
-		'https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap',
-		array(),
-		null
-	);
-	wp_enqueue_style(
-		'business-setup-google-fonts-montserrat',
-		'https://fonts.googleapis.com/css2?family=Montserrat:wght@400&display=swap',
-		array(),
-		null
-	);
-	wp_enqueue_style(
-		'business-setup-google-fonts-open-sans',
-		'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400&display=swap',
-		array(),
-		null
-	);
-	// Add custom font-face CSS
-	wp_add_inline_style(
-		'business-setup-output',
-		"
-        @font-face {
-            font-family: 'Eudoxus Sans';
-            src: url('" . get_template_directory_uri() . "/web/public/EudoxusSans-Regular-BF659b6cb1d4714.ttf') format('truetype');
-            font-weight: 400;
-        }
-        @font-face {
-            font-family: 'Eudoxus Sans';
-            src: url('" . get_template_directory_uri() . "/web/public/EudoxusSans-Medium-BF659b6cb1c14cb.ttf') format('truetype');
-            font-weight: 500;
-        }
-        @font-face {
-            font-family: 'Eudoxus Sans';
-            src: url('" . get_template_directory_uri() . "/web/public/EudoxusSans-Bold-BF659b6cb1408e5.ttf') format('truetype');
-            font-weight: 700;
-        }
-        "
 	);
 
 	wp_register_style( 'starter-navigation', get_template_directory_uri() . '/assets/css/components/header/navigation.css', array(), time() );

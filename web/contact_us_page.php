83397c0bdbfaf588ec0d01e88e7a6f5ffdb2473e
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-09-21 23:43:02 +0500

    menu

--- web/contact_us_page.php
+++ web/contact_us_page.php
@@ -4,12 +4,65 @@
 ?>
 <body>
 <div
-		class="w-full relative flex flex-col items-start justify-start leading-[normal] tracking-[normal]"
+		class="w-full"
 >
-<?php
-	include_once ('menu.php');
-
+	<?php
+	include_once( 'menu.php' );
 	?>
+
+	<section class="self-stretch bg-background-surface-neutral overflow-hidden flex flex-col items-start justify-start relative max-w-full text-center text-41xl text-text-base-primary font-body-s-bold">
+		<div class="self-stretch flex flex-col items-center justify-center pt-measurements-gap-160-96 px-5 pb-measurements-gap-120 box-border max-w-full mq450:pt-measurements-gap-120 mq450:pb-measurements-gap-120 mq450:box-border mq1050:pt-measurements-gap-120 mq1050:pb-measurements-gap-120 mq1050:box-border">
+			<div class="max-w-[1200px] flex flex-col items-center justify-start gap-numbers-40 w-full mq750:gap-numbers-40">
+				<div class="cursor-pointer border-border-base-alpha border-[1px] border-solid py-0.5 px-[5px] bg-background-badge-blue h-7 rounded-measurements-radius-full box-border flex flex-row items-center justify-center gap-numbers-4">
+										<div class="flex flex-row items-center justify-center">
+						<div class="flex-1 flex flex-row items-center justify-center py-0 px-[3px]">
+							<div class="flex-1 relative text-sm tracking-[-0.1px] leading-[20px] font-medium font-body-s-bold text-text-accent-blue-inverted text-center inline-block min-w-[74px]">
+								Contact us
+							</div>
+						</div>
+
+					</div>
+				</div>
+				<div class="self-stretch flex flex-col items-center justify-start gap-numbers-24 max-w-full">
+					<h1 class="m-0 relative text-inherit tracking-[-1.7px] leading-[66px] font-medium font-inherit inline-block max-w-[792px] mq450:text-17xl mq450:leading-[40px] mq750:text-29xl mq750:leading-[53px] mq1050:max-w-full">
+						Speak with Our Business Setup Consultants in Dubai/UAE
+					</h1>
+					<div class="m-0 relative text-xl tracking-[-0.2px] leading-[28px] font-normal font-inherit text-text-base-secondary mq450:text-base mq450:leading-[22px]">
+						Book a free consultation with one of our Company Formation
+						Specialists now and find out all&nbsp;the&nbsp;information you need about
+						starting your own company in the UAE.
+					</div>
+				</div>
+				<div class="self-stretch flex flex-row items-start justify-start flex-wrap content-start gap-x-[34px] gap-y-10 max-w-full text-left text-base text-text-base-secondary mq750:gap-[17px]">
+					<div class="flex-1 relative tracking-[-0.2px] leading-[24px] inline-block min-w-[342px] max-w-full">
+						<p class="m-0">
+							Let no one tell you otherwise—starting a new business in Dubai
+							isn’t something you want to tackle solo. It can be very
+							soul-crushing, with moments of head-scratching confusion and a
+							mountain of regulatory and admin hurdles to jump over.
+						</p>
+						<p class="m-0">&nbsp;</p>
+						<p class="m-0">
+							To make things smoother, you need expert business setup
+							consultants by your side. That’s where we at Vision Prime come
+							in.
+						</p>
+					</div>
+					<div class="flex-1 relative tracking-[-0.2px] leading-[24px] inline-block min-w-[342px] max-w-full">
+						With over a decade of consultancy experience, our team knows the
+						UAE business scene inside and out. We offer tailored services
+						for everything from&nbsp;company registration and licensing to visa
+						processing and financial services. We handle it all with
+						precision and efficiency, so you can focus on&nbsp;what you do
+						best—growing your business.
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</section>
+
+
 	<section
 			class="self-stretch bg-background-surface-default flex flex-col items-center justify-start max-w-full text-center text-sm text-text-accent-blue-inverted font-body-s-bold"
 	>

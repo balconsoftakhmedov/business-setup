d7df4898ec8200e0712c649d6b338b05632f8b82
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-09-21 21:04:08 +0500

    header

--- templates/header/parts/menu.php
+++ templates/header/parts/menu.php
@@ -1,9 +1,9 @@
 <div class="header-placeholder hidden h-[90px] mq950:block "></div>
 <header
-		class="stm-h stm_menu-sticky self-stretch  er-back-tr coltr  mq1170:w-full
+		class="stm-h stm_menu-sticky self-stretch  er-back-tr coltr
 		flex flex-row items-center justify-center py-[15px] box-border
 		top-[0] z-[11111] sticky mq950:fixed mq780: max-w-full gap-5
-		text-right text-base text-white font-subtitles-16  lg:box-border  mq450:box-border mq850:box-border"
+		text-right text-base text-white font-subtitles-16  lg:box-border  mq450:box-border mq850:box-border mq1170:w-full"
 >
 	<div class="max-w-[1200px] flex flex-row items-end justify-between w-full mq650:pl-[20px] mq650:pr-[10px]
 	mq950:pl-[20px] items-center mq950:pr-[10px]

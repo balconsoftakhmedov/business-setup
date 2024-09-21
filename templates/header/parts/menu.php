e14be1b11780962b2d34cb657cf61e2a0f6028e7
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-09-21 23:25:12 +0500

    Update menu.php

--- templates/header/parts/menu.php
+++ templates/header/parts/menu.php
@@ -1,10 +1,8 @@
 <div class="header-placeholder hidden h-[90px] mq950:block "></div>
-<header
-		class="stm-h stm_menu-sticky self-stretch  er-back-tr coltr
+<header class="stm-h stm_menu-sticky self-stretch  er-back-tr coltr stm-menu-colors
 		flex flex-row items-center justify-center py-[15px] box-border
 		top-[0] z-[11111] sticky mq950:fixed mq780: max-w-full gap-5
-		text-right text-base text-white font-subtitles-16  lg:box-border  mq450:box-border mq850:box-border mq1170:w-full"
->
+		text-right text-base text-white font-subtitles-16  lg:box-border  mq450:box-border mq850:box-border mq1170:w-full">
 	<div class="max-w-[1200px] flex flex-row items-end justify-between w-full mq650:pl-[20px] mq650:pr-[10px]
 	mq950:pl-[20px] items-center mq950:pr-[10px]
 	mq850:items-center">
@@ -24,7 +22,7 @@
 					// Group menu items by their parent IDs
 					$menu_item_parents[ $menu_item->menu_item_parent ][] = $menu_item;
 				}
-				echo '<div class="flex text-center text-sm text-background-surface-default font-body-s-bold flex-row items-center justify-center
+				echo '<div class="flex text-center text-sm stm-menu-colors font-body-s-bold flex-row items-center justify-center
 		py-5 px-0 box-border gap-[30px]
 		max-w-full mq1024:hidden mq850:gap-5">';
 				foreach ( $menu_item_parents[0] as $menu_item ) { // Start with the top-level items
@@ -37,12 +35,16 @@
 						<a class="[text-decoration:none] relative tracking-[0.04em] leading-[18px]  font-medium text-[inherit] whitespace-nowrap hover:font-bold " href="<?php echo $url ?>">
 							<?php echo $title ?>
 							<?php if ( $has_children ): ?>
-								<img class="inline-block h-[18px] w-[18px] relative overflow-hidden shrink-0 min-h-[18px]
-transform transition-transform duration-300 group-hover:rotate-180" src="/wp-content/themes/business-setup/web/public/arrowdownsline1.svg" alt="Submenu Icon"/>
+									<img class="inline-block stm-logo-white  h-[18px] w-[18px] relative overflow-hidden shrink-0 min-h-[18px]
+transform transition-transform duration-300 group-hover:rotate-180" src="/wp-content/themes/business-setup/web/public/arrowdownsline1.svg" alt="Submenu Icon">
+
+							<img class="inline-block stm-logo-black h-[18px] w-[18px] relative overflow-hidden shrink-0 min-h-[18px]
+transform transition-transform duration-300 group-hover:rotate-180" src="/wp-content/themes/business-setup/web/public/arrowdownsline.svg" alt="Submenu Icon">
+
 							<?php endif ?>
 						</a>
 						<?php if ( $has_children ): ?>
-							<div class="absolute right-[0px] hidden mt-2 bg-general-1-primary shadow-lg rounded-md w-48 group-hover:block z-10 w-full text-left min-h-[100px] top-[8px] er-back-tr">
+							<div class="absolute right-[0px]  stm-dropdown   hidden mt-2 bg-general-1-primary shadow-lg rounded-md w-48 group-hover:block z-10 w-full text-left min-h-[100px] top-[8px] er-back-tr">
 								<ul class="py-2">
 									<?php foreach ( $menu_item_parents[ $menu_item->ID ] as $child ): ?>
 										<li>

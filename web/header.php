b29579044f5966c0e25a084c0ebcae4a803947d9
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-09-21 22:52:48 +0500

    fix

--- web/header.php
+++ web/header.php
@@ -5,10 +5,12 @@
 	<meta name="viewport" content="initial-scale=1, width=device-width"/>
 
 	<link rel="stylesheet" href="./output.css"/>
+	<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
 	<script src="./homepage.js" defer></script>
 	<script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/intersect@3.x.x/dist/cdn.min.js"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script defer src="/wp-content/themes/business-setup/assets/js/components/header/header.js"></script>
+
  <link
       rel="stylesheet"
       href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
